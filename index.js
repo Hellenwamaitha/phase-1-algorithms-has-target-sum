e8d55998ad0782f63ecd12ddf0bf4c3d70354a91
hellenwamaitha <hellenwama254@gmail.com>
2023-07-02 15:34:16 +0300

    assignment

--- index.js
+++ index.js
@@ -17,6 +17,27 @@ function hasTargetSum(array, target) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  hasTargetSum([3, 8, 12, 4, 11, 7], 20);
+// returns true, since 12 and 8 add up to 20
+
+hasTargetSum([22, 19, 4, 6, 30], 10);
+// returns true, since 4 and 6 add up to 10
+
+hasTargetSum([1, 2, 5], 8);
+// returns false, since no pair of numbers adds up to 8
+console.log("Expecting: true");
+  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 20));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 10));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 2, 5], 8));
+
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
